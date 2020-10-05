65b85b9dca6285512ea1b475b4bb52aded054190
unknown <mittalshubham05@gmail.com>
2020-10-05 21:13:59 +0530

    cheapest hotel with best rating

---
+++ hotelReservation/src/main/java/bridgelabz/hotelReservation/CustomComparator.java
@@ -0,0 +1,15 @@
+package bridgelabz.hotelReservation;
+
+import java.util.Comparator;
+
+public class CustomComparator implements Comparator<Integer>{
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		if(o1 > o2 )
+			return 1;
+		else if(o1 < o2)
+			return -1;
+		else
+			return 0;
+	}
+}
